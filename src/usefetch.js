c019a29fbf4cc6a80ad89e41c1480a0274fd8aaf
Nyandika Donald <donald.nyandika@gmail.com>
2021-08-26 20:41:55 +0300

    Fix axios import

--- src/usefetch.js
+++ src/usefetch.js
@@ -1,34 +1,34 @@
+import axios from 'axios';
 import { useEffect, useState } from 'react';
-import axios from axios;
 
-const useFetch = (url) =>{
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
-    const [data, setData] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
+  useEffect(() => {
+    setTimeout(() => {
+      axios
+        .get(url)
+        .then((data) => {
+          console.log(data);
+          setData(data);
+          setIsPending(false);
+          setError(null);
+          console.log('here');
+        })
+        .catch((err) => {
+          console.log(err);
+          if (err.name === 'AbortError') {
+            console.log('fetch aborted');
+          } else {
+            setIsPending(false);
+            setError(err.message);
+          }
+        });
+    }, 1000);
 
-    useEffect(() => {
-        setTimeout(()=>{
-            axios.get(url).then((data)=>{
-                console.log(data);
-                  setData(data);
-                  setIsPending(false);
-                  setError(null);
-                  console.log('here');
-            })
-            .catch((err)=>{
-                console.log(err);
-                if (err.name ==='AbortError'){
-                    console.log('fetch aborted');
-                }
-                else {
-                  setIsPending(false);
-                  setError(err.message);
-                }
-            })
-        }, 1000);
-
-        return() => abortCont.abort();
-    },[url]);
-}
+    return () => abortCont.abort();
+  }, [url]);
+};
 export default useFetch;
